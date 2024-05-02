d18e682291403615e9cdd9c1c49f6b8cad112060
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-05-02 18:01:19 +0100

    init

---
+++ update.ps1
@@ -0,0 +1,12 @@
+Invoke-WebRequest -Uri https://github.com/microsoft/cppwinrt/releases/download/2.0.240405.15/Microsoft.Windows.CppWinRT.2.0.240405.15.nupkg -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile cppwinrt.7z
+7z.exe x -y cppwinrt.7z -ocppwinrtbin
+
+Invoke-WebRequest -Uri  https://github.com/microsoft/windows-rs/raw/master/crates/libs/bindgen/default/Windows.winmd -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile Windows.winmd
+
+Move-Item -Force .\cppwinrtbin\bin\cppwinrt.exe .
+.\cppwinrt.exe -in '.\Windows.winmd' -out '.\include'
+
+Remove-Item -Force cppwinrt.7z
+Remove-Item -Force cppwinrt.exe
+Remove-Item -Force Windows.winmd
+Remove-Item -Recurse -Force .\cppwinrtbin
